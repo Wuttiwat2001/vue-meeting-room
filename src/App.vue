e3ee9b5401b9d6a78ef4c099561811ea3a27795a
KhunCaptain <captenlnw_za@hotmail.com>
2024-12-10 00:26:07 +0700

    Use Vue Router

--- src/App.vue
+++ src/App.vue
@@ -11,6 +11,10 @@ onMounted(() => {
 
 <template>
   <div>
+    <nav>
+      <router-link to="/">Home</router-link>
+      <router-link to="/register">Register</router-link>
+    </nav>
     <router-view></router-view>
   </div>
 
--- src/router.js
+++ src/router.js
@@ -1,14 +1,16 @@
-import { createMemoryHistory, createRouter } from 'vue-router'
+import { createMemoryHistory, createRouter } from "vue-router";
 
-import Home from './views/Home.vue'
+import Home from "./views/Home.vue";
+import Register from "./views/Register.vue";
 
 const routes = [
-  { path: '/', component: Home },
-]
+  { path: "/", component: Home },
+  { path: "/register", component: Register },
+];
 
 const router = createRouter({
   history: createMemoryHistory(),
   routes,
-})
+});
 
 export default router;
